ee0604def9f00da28fe6505194e3270ea34a2d86
valeriisabadash-ctrl <valerii.sabadash@icloud.com>
2026-07-16 12:28:42 -0700

    Bust mobile cache for redesigned phone layout

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
   if (!existingMobileStyles) {
     const mobileStyles = document.createElement('link');
     mobileStyles.rel = 'stylesheet';
-    mobileStyles.href = 'mobile.css?v=20260716-4';
+    mobileStyles.href = 'mobile.css?v=20260716-5';
     mobileStyles.dataset.mobileLayout = 'true';
     document.head.appendChild(mobileStyles);
   }
